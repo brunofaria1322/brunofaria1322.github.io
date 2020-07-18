8894eb82ce0dd7e7d688c14f1fa8714f7135b830
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2020-07-18 12:15:33 +0100

    scrolling with bugs

--- main.js
+++ main.js
@@ -7,4 +7,26 @@
 
 function main(){
 
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    let scrolling= false;
+
+    function scroll( down){
+        window.scrollBy(0, down? -vh : vh);
+    }
+
+    function scrollHandler(ev) {
+        console.log(ev.deltaY);
+        if (scrolling) {
+            if (!ev.deltaY){    //scrolling ends with delta = 0 or -0 / prevents from double scrolling
+                
+                console.log("pipi parou");
+                scrolling = false;
+            }
+        }
+        else{
+            scrolling=true;
+            scroll(ev.deltaY<0? true : false)   //true if down, false if up
+        }
+    }
+    window.addEventListener('wheel', scrollHandler);
 }
