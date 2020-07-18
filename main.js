63de0335a3cd9eab412c6437bbbf3ffcc24c53e3
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2020-07-19 00:18:24 +0100

    title font changing

--- main.js
+++ main.js
@@ -8,29 +8,29 @@
 function main(){
 
     let secs = ["Home","About", "Skills", "Work", "Contacts"];
+    for (let i= 0; i<secs.length; i++){
+        secs[i]=document.getElementById(secs[i])
+    }
     let current_sec=0;
     let scrolling= false;
 
     function scroll(down){
         if (down){
-            if (current_sec--<0){               //if first section
+            if (--current_sec<0){               //if first section
                 current_sec=0;
                 return;
             }
         }
         else{
-            if (current_sec++>secs.length-1){   //if last section
+            if (++current_sec>secs.length-1){   //if last section
                 current_sec=secs.length-1;
                 return;
             }
         }
-        var element_to_scroll_to = document.getElementById(secs[current_sec]);
-        element_to_scroll_to.scrollIntoView({behavior: 'smooth' });
-        //location.hash = "#" + secs[current_sec];
+        secs[current_sec].scrollIntoView({behavior: 'smooth' });
     }
 
     function scrollHandler(ev) {
-        console.log(ev.deltaY);
         if (scrolling) {
             if (!ev.deltaY){    //scrolling starts with deltaY = 0 or -0 / prevents from double scrolling
                 
@@ -44,4 +44,5 @@ function main(){
         }
     }
     window.addEventListener('wheel', scrollHandler);
+
 }
