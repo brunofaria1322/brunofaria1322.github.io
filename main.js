e2c287efa662abebd62f36d62fed73b6dddc6c6f
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2020-07-23 23:15:03 +0100

    scrolling working beautifully (can improve)

--- main.js
+++ main.js
@@ -7,42 +7,4 @@
 
 function main(){
 
-    let secs = ["Home","About", "Skills", "Work", "Contacts"];
-    for (let i= 0; i<secs.length; i++){
-        secs[i]=document.getElementById(secs[i])
-    }
-    let current_sec=0;
-    let scrolling= false;
-
-    function scroll(down){
-        if (down){
-            if (--current_sec<0){               //if first section
-                current_sec=0;
-                return;
-            }
-        }
-        else{
-            if (++current_sec>secs.length-1){   //if last section
-                current_sec=secs.length-1;
-                return;
-            }
-        }
-        
-        console.log(secs[current_sec]);
-        secs[current_sec].scrollIntoView({block: 'start', behavior: 'smooth' });
-    }
-
-    function scrollHandler(ev) {
-        if (scrolling) {
-            if (!ev.deltaY){    //scrolling starts with deltaY = 0 or -0 / prevents from double scrolling
-                scrolling = false;
-            }
-        }
-        else{
-            scrolling=true;
-            scroll(ev.deltaY<0? true : false)   //true if down, false if up
-        }
-    }
-    window.addEventListener('wheel', scrollHandler);
-
 }
