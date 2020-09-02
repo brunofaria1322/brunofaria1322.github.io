d758ac9edae8e48ddea8148e1a9708830342e970
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2020-09-02 12:24:36 +0100

    form test 4

--- formHandler.php
+++ formHandler.php
@@ -1,19 +1,16 @@
 <?php
     $name = $_POST["name"];
-    $visitor_email = $_POST["email"];
+    $email = $_POST["email"];
     $message = $_POST["message"];
 
     $subject = "New Form Submission";
+    $formcontent="From: $name \n Message: $message";
         
     $to = "brunofaria1322@gmail.com";
 
-    $headers ="From: " . $name . "<". $visitor_email .">\r\n";
+    $mailheader = "From: $email \r\n";
     
-    if(mail($to, $subject, $message, $headers)){
-	    $out_message = "Your contact information is received successfully.";
-	    $type = "success";
-    }
-
-    require_once "formHandler.php";
+    mail($to, $subject, $formcontent, $mailheader)  or die("Error!");
+    echo "Thank You!";
 
 ?>
